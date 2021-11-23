6abae488c6ede18174576f547ce35b3f0c2f6e2a
Patlund <patrik.ojerssonlundgren@gmail.com>
2021-11-23 08:01:39 +0100

    Redid the project in a new HTML file, index2.html is the startpage

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function () {
+  $("#test").click(function () {
+    alert('Sug röv');
+  });
+});
+
+let nameValue = document.getElementById("#current_savings_form").value;
